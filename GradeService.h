6f9577fc2cb8e1ed0ee0fee04976447e635fc15b
Yeonsoo So <65909443+yeonsooSo@users.noreply.github.com>
2020-09-06 23:35:47 +0900

    Update GradeService.h
    class 함수 정의 다른 소스파일로

--- GradeService.h
+++ GradeService.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iomanip>
 #include <iostream>
-//"1. 분포도 2. 총원 3. 합격자 총원 4. 합격자 평균 5. 전체 평균 6. 종료"
+
 class GradeService {
 public:
 	void distribution(int a, int b);
@@ -9,21 +9,3 @@ public:
 	double totalAve(int a, int b);
 
 };
-void distribution(int a, int b) {
-	std::cout << "Pass: " << std::setw(3) << a << " ";		
-	for (int i{ a }; i > 0; i--) {					
-		std::cout << "*";
-	}
-	std::cout << "\nFail: " << std::setw(3) << b << " ";		
-	for (int i{ b }; i > 0; i--) {					
-		std::cout << "*";
-	}
-	std::cout << "\n\n";
-}
-
-double passAve(int a, int b) {
-	return static_cast<double>(a) / b;
-}
-double totalAve(int a, int b) {
-	return static_cast<double>(a) / b;
-}
